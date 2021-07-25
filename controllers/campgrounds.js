e23ab409349e75b916b4084fccd7ad23ee37b762
Sahar <sahar31872@gmail.com>
2021-07-26 01:59:29 +0300

    fix some location update issues

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -54,9 +54,16 @@ module.exports.renderEditForm = async (req, res) => {
 }
 
 module.exports.updateCampground = async (req, res) => {
+    const geoData = await geocoder.forwardGeocode({
+        query: req.body.campground.location,
+        limit: 1
+    }).send()
+    console.log(geoData.body.features[0].geometry);
+
     const { id } = req.params;
-    console.log(req.body);
+    req.body.campground.geometry = geoData.body.features[0].geometry;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    //campground.geometry = geoData.body.features[0].geometry;
     const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }));
     campground.images.push(...imgs)
     await campground.save();
